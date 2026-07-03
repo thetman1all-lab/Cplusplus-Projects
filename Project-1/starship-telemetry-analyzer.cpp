a26f6d7ab26083ec647b46c1c3447c40eb0be748
tcounihan1 <thetman1all@gmail.com>
2026-07-03 07:42:59 -0700

    Working through the code. Still has bugs

--- Project-1/starship-telemetry-analyzer.cpp
+++ Project-1/starship-telemetry-analyzer.cpp
@@ -16,20 +16,47 @@ int main() {
     }
 
     std::string line;
+    int line_num = 0; // Keep track of line number
+    std::map<std::string, std::vector<double>> sensor_data;   // declare this BEFORE the while loop
 
     while (std::getline(infile, line)) {          // full line
-        if (line.empty() || line[0] == '#') continue;
 
+        line_num++;
+
+        if (line.empty() || line[0] == '#') {
+            // skipped++;
+            continue;
+        }
         std::stringstream ss(line);
-        std::string timestamp, sensor, value_str, unit, status;
+
+        std::string timestamp, sensor, value_str, unit, status; // Fresh variables every iteration
+
         std::getline(ss, timestamp, ',');
         std::getline(ss, sensor, ',');
         std::getline(ss, value_str, ',');
         std::getline(ss, unit, ',');
         std::getline(ss, status, ',');
+
+        std::cout << value_str << "\n" << !!std::getline(ss, value_str, ',') << "\n";
+
+        if (!std::getline(ss, timestamp, ',') || !std::getline(ss, sensor, ',') || !std::getline(ss, value_str, ',') || !std::getline(ss, unit, ',') || !std::getline(ss, status, ',')) {
+            std::cerr << "Warning: malformed line " << line_num << "\n";
+            continue;
+        }
+        
+        double value; // Define fresh value to store the conversion of the string to a double
+
+        try {
+            value = std::stod(value_str);
+        } catch (...) {
+            std::cerr << "Warning: bad numeric value on line " << line_num << "\n";
+            continue;
+        }
+
+        // === HERE is where the data STORED ===
+        sensor_data[sensor].push_back(value);     // <--- this is the key line where data is now stored for the report
     }
 
     infile.close();
-
     return 0;
 }
