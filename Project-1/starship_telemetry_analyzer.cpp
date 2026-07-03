3a6356c703c16a61babaad633467d08418b40a95
tcounihan1 <thetman1all@gmail.com>
2026-07-03 06:39:40 -0700

    Added first couple of parts to read file

--- Project-1/starship_telemetry_analyzer.cpp
+++ Project-1/starship_telemetry_analyzer.cpp
@@ -8,5 +8,28 @@
 #include <iomanip>
 
 int main() {
+    std::ifstream infile("/Users/wardkimball/Desktop/Misc/Computing/Cplusplus-Projects/Project-1/starship_avionics_log.txt");
+
+    if (!infile.is_open()) {
+        std::cerr << "Error: Cannot open input file\n";
+        return 1;
+    }
+
+    std::string line;
+
+    while (std::getline(infile, line)) {          // full line
+        if (line.empty() || line[0] == '#') continue;
+
+        std::stringstream ss(line);
+        std::string timestamp, sensor, value_str, unit, status;
+        std::getline(ss, timestamp, ',');
+        std::getline(ss, sensor, ',');
+        std::getline(ss, value_str, ',');
+        std::getline(ss, unit, ',');
+        std::getline(ss, status, ',');
+    }
+
+    infile.close();
+
     return 0;
 }
