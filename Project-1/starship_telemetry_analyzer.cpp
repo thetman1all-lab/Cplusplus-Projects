a8a2042c69fb4150674f23c34076879a63a58009
tcounihan1 <thetman1all@gmail.com>
2026-07-03 06:29:38 -0700

    Added data file and renamed the cpp file

---
+++ Project-1/starship_telemetry_analyzer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+int main() {
+    return 0;
+}
