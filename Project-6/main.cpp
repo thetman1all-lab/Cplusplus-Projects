53779e4fd5bad9bb073a09bcd3d3c783cc81a37b
tcounihan1 <thetman1all@gmail.com>
2026-07-06 13:03:59 -0700

    Main is now done. Ready to be tested.

---
+++ Project-6/main.cpp
@@ -0,0 +1,15 @@
+#include "SensorReading.h"
+#include "TBMMonitor.h"
+#include <iostream>
+
+int main() {
+    TBMMonitor classUser; // Create the TBMMonitor as an object stored as classUser
+
+    classUser.loadFromFile("boring_tbm_sensor_data.txt"); // Use the loadFromFile() function to load, read, and store the data from the desired data file
+    
+    classUser.generateCriticalReport("TMB_Critical_Report.txt"); // Use the generateCriticalReport() function to generate the report titled with the inputed name
+
+    classUser.printSummary(); // Use the printSummary() function to print a brief summary to the terminal
+
+    return 0;
+}
